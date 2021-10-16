484fbc34757ad93736f89d3f4f8979490663a517
Scuttle <gustav@almstrom.org>
2021-10-16 21:27:06 +0200

    hit and shit

--- bot.py
+++ bot.py
@@ -286,6 +286,7 @@ async def on_message(message):
                 await message.channel.send(string_streampw_denied)
         #Hit or shit
         if any(message.content.startswith(word) for word in wordlist_hitorshit):
+            user_longuid = "<@" + str(user_longuid) + ">"
             if message.content.startswith('!hit'):
                 #print('fart')
                 add_success, str_addmessage, added_songid = gbsfm.gbsfm_play('highrating', user_gbsfmid, user_apikey, user_longuid, 4.8)
