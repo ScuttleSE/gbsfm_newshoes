53008b370cec9c7053fc28ea40a7f8d02d4f6a1a
Gustav Almstrom <gustav@almstrom.org>
2023-07-01 10:34:15 +0200

    undo added dongs

--- gbsfm.py
+++ gbsfm.py
@@ -759,15 +759,14 @@ def gbsfm_undo( user_gbsfmid, user_longuid ):
     db.commit()
     queryresult = query.fetchone()
     print(queryresult)
-    print(queryresult[0][0])
-    print(queryresult[0][1])
-    print(queryresult[0][2])
-    print(queryresult[0][3])
-    print(queryresult[0][4])
-    print(queryresult[0][5])
-    print(queryresult[0][6])
-    print(queryresult[0][7])
-    print(queryresult[0][8])
+    print(queryresult[0])
+    print(queryresult[1])
+    print(queryresult[2])
+    print(queryresult[3])
+    print(queryresult[4])
+    print(queryresult[5])
+    print(queryresult[6])
+    print(queryresult[7])
     if queryresult == None:
         msg = user_longuid + "does not have any upcoming songs on the playlist."
     elif queryresult[5] == 1:
